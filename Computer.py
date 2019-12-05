4953cc0fc76adff35860c78c4cc7bca8af88c0ca
Morten <modown098@gmail.com>
2019-12-05 21:45:56 +0100

    Added Test library with RNGtest

--- Computer.py
+++ Computer.py
@@ -1,21 +1,6 @@
-import matplotlib.pyplot as plt
-from binaryRNG import RNG
 import numpy as np
+from Test import RNGtest
 
-def RNGtest():
-    numbers = []
-    bits = 8
-    for x in range(1000):
-        numbers.append(RNG(bits))
-
-
-    plt.figure(1)
-    plt.hist(numbers,bins=range(min(numbers),max(numbers)+10,10))
-    plt.grid(True)
-    #plt.show()
-
-    print(numbers.count(0),numbers.count(255))
-
-#Something is wrong with the amount of 0 values compared to 255 values
-#It should be just as likely to get 0 as 255 (0 is all 8 bits being 0 and 255 is all 8 bits being 1)
+# There is a 1/4 chance that each bit will be 1, and 3/4 chance that the bit will be zero
+# This is the reason for the scewed distribution towards lower numbers
 RNGtest()
--- binaryRNG.py
+++ binaryRNG.py
@@ -4,7 +4,7 @@ import numpy as np
 def RNG(numberOfBits):
     bunch = []
     for x in range(numberOfBits):
-        # 8-bit list
+        # x-bit list
         rand = np.random.randint(2,size=2)
         bunch.append(Transistor(rand[0],rand[1]))
 
