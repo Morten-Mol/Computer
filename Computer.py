bd3601ceb1a783f7ea927d7d026081da29531342
Morten <modown098@gmail.com>
2019-12-08 16:32:19 +0100

    OR-gate

--- Computer.py
+++ Computer.py
@@ -4,3 +4,7 @@ if __name__ == '__main__':
     print(Gate(0,0).AND())
     print(Gate(1,0).AND())
     print(Gate(1,1).AND())
+
+    print(Gate(0,0).OR())
+    print(Gate(1,0).OR())
+    print(Gate(1,1).OR())
--- Gate.py
+++ Gate.py
@@ -7,8 +7,18 @@ class Gate:
         self.iTop = iTop
         self.iBot = iBot
 
-    # An AND-gate can simply be modelled as an transistor
+    # An AND-gate can simply be modelled as two tranistors in series
+    # The drain of the t1 transistor is passed at the source for the
+    # t2 transistor
     def AND(self):
-        t1 = Transistor(1,self.iTop)
-        t2 = Transistor(t1.drain,self.iBot)
-        return t2.drain
+        t1_AND = Transistor(1,self.iTop)
+        t2_AND = Transistor(t1_AND.drain,self.iBot)
+        return t2_AND.drain
+
+    # OR-gate made out of 2 transistors. The drain from both are
+    # compared and 1 is parsed as the output if any one of them
+    # are 1
+    def OR(self):
+        t1_OR = Transistor(1,self.iTop)
+        t2_OR = Transistor(1,self.iBot)
+        return max((t1_OR.drain,t2_OR.drain))
