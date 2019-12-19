f54633735afc256ffd4861fe5bd4dc718bb8692d
Morten <modown098@gmail.com>
2019-12-19 19:06:52 +0100

    Truthtable generation for all implemented gates - XNOR missing

--- TestFunctions/GateLogicCheck.py
+++ TestFunctions/GateLogicCheck.py
@@ -1,1 +1,45 @@
-# Add tests for all logic checks of all types of logic gates
+from Gate import Gate
+
+def TestGateTruth(gateType):
+    """
+    Creates truth table for given gateType
+
+    Input: String - AND, OR etc.
+    """
+
+    # All possible inputs for the gates
+    Inputs = [(0,0), (0,1), (1,0), (1,1)]
+
+
+    print('---%s - Truthtable---' % gateType)
+
+    # Iterate over all posible binary inputs
+    for i in Inputs:
+        TGate = Gate(i[0],i[1])
+
+        if gateType == 'AND':
+            output = TGate.AND()
+            print('Input: %d, %d | Output: %d' % (i[0],i[1],output))
+
+        if gateType == 'OR':
+            output = TGate.OR()
+            print('Input: %d, %d | Output: %d' % (i[0],i[1],output))
+
+        if gateType == 'NOT':
+            output = TGate.NOT()
+            print('Input: %d, %d | Output: %d' % (i[0],i[1],output))
+
+        if gateType == 'NAND':
+            output = TGate.NAND()
+            print('Input: %d, %d | Output: %d' % (i[0],i[1],output))
+
+        if gateType == 'NOR':
+            output = TGate.NOR()
+            print('Input: %d, %d | Output: %d' % (i[0],i[1],output))
+
+        if gateType == 'XOR':
+            output = TGate.XOR()
+            print('Input: %d, %d | Output: %d' % (i[0],i[1],output))
+
+    # Spacer for next TestGateTruth call in Tests.py
+    print('')
--- Tests.py
+++ Tests.py
@@ -1,4 +1,11 @@
 from TestFunctions.BinaryRNG import BinaryRNG_Test
+from TestFunctions.GateLogicCheck import TestGateTruth
 
 # Tests functions to run
 BinaryRNG_Test(8,1e4)
+
+# Gate truthtable generator
+gateTypes = ['AND', 'OR', 'NOT', 'NAND', 'NOR','XOR']
+print('Cross-refernce following table with truth tables at https://www.tutorialspoint.com/computer_logical_organization/logic_gates.htm')
+for gate in gateTypes:
+    TestGateTruth(gate)
