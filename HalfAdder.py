d2dd907422ccc10f878ae20f0ae9a6c4efb58157
Morten <modown098@gmail.com>
2020-01-16 19:15:16 +0100

    HalfAdder init

---
+++ HalfAdder.py
@@ -0,0 +1,18 @@
+from Gate import Gate
+
+class HalfAdder():
+# Half adder
+#
+# Simple addition of two binary numbers
+#
+# Input : Top bit , bottom bit
+# Output : Sum and Carry of operation
+
+    def __init__(self,tI,bI):
+        self.topInput = tI
+        self.botInput = bI
+        self.sum = s
+        self.carry = car
+
+if __name__ == "__main__" :
+    print(Gate(0,0).AND())
